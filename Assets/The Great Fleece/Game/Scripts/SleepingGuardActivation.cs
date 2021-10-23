a038544d1422108e08d157f076c08934af21105a
Samarth Dhroov <samarth.dhroov@hotmail.com>
2021-10-23 11:41:28 +0530

    Sleeping Guard Interaction in place. Working Fine.

---
+++ Assets/The Great Fleece/Game/Scripts/SleepingGuardActivation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepingGuardActivation : MonoBehaviour
+{
+    public GameObject sleepingGuardCutScene;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            sleepingGuardCutScene.SetActive(true);
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+}
